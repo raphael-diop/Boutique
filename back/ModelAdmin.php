33c955900168edb91c3ae4b42f392359b3234694
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-04-26 13:05:46 +0200

    model doc et modeladmin

--- back/ModelAdmin.php
+++ back/ModelAdmin.php
@@ -17,6 +17,22 @@ class Admin extends Model{
         return $getDocs;
     }
 
+    public function getOneDoc($id){
+        $getDoc = $this->bdd->prepare("SELECT * FROM `documents` WHERE `id` = $id");
+        $getDoc->execute();
+        $getOneDoc = $getDoc->fetchall(PDO::FETCH_ASSOC);
+        //var_dump($getDoc);
+        return $getOneDoc;
+    }
+
+    public function modifOneDoc($id, $contenu){
+
+        $contenu = addslashes($contenu);
+        //$this->bdd->quote($contenu);
+        $modifDoc = $this->bdd->prepare("UPDATE documents SET text = '$contenu' WHERE id = $id");
+        $modifDoc->execute();
+    }
+
     public function getAllProd(){
         $getProd = $this->bdd->prepare("SELECT * FROM `produits`");
         $getProd->execute();
@@ -30,6 +46,111 @@ class Admin extends Model{
         $getProduit = $getOneProd->fetchall(PDO::FETCH_ASSOC);
         return $getProduit;
         }
+
+    public function modifOneProd($id, $prix, $title, $descriptif, $categorie){
+        $prix = addslashes($prix);
+        $title = addslashes($title);
+        $descriptif = addslashes($descriptif);
+        $modifProd = $this->bdd->prepare("UPDATE produits SET prix = '$prix', titre = '$title', descriptif = '$descriptif', id_categorie = '$categorie'  WHERE id = $id");
+        $modifProd->execute();
+        $modifOneProd = $modifProd->fetchall(PDO::FETCH_ASSOC);
+        return $modifOneProd;
+        }
+
+    public function addOneProd($prix, $title, $descriptif, $categorie){
+        $prix = addslashes($prix);
+        $title = addslashes($title);
+        $descriptif = addslashes($descriptif);
+        $request = "SELECT titre FROM `produits` WHERE titre = '$title'";
+        $calcul = $this->bdd->prepare($request);
+        $calcul -> execute();
+        $result = $calcul->rowCount();
+        //var_dump($calcul);
+
+        if(($result) == 1){
+        $messNewProd = "Le nom du produit doit être unique, réessayez avec un autre nom.";
+    }
+        else {
+            $requetesql1 = "INSERT INTO `produits` (`prix`, `titre`, `descriptif`, `id_categorie`) VALUES ('$prix', '$title', '$descriptif', '$categorie')";
+        $calcul1 = $this->bdd->prepare($requetesql1);
+        $calcul1 -> execute();
+        $messNewProd = "Première étape réussi";
+        }
+        return $title;
+    
+
+        }
+
+    public function imgProd($name, $dataImage, $data, $typeimage){
+        echo $name;
+        $calculid = $this->bdd->prepare("SELECT id FROM `produits` WHERE titre = '$name'");
+        $calculid -> execute();
+        $id_Art = $calculid->fetchall(PDO::FETCH_ASSOC);
+        $idArt = $id_Art['0']['id'];
+
+
+        if(mime_content_type($dataImage['img_file']) == "image/jpeg" || mime_content_type($dataImage['img_file']) == "image/png" || mime_content_type($dataImage['img_file']) == "image/webp") {
+            move_uploaded_file($dataImage['img_file'], $dataImage['img_link']); //https://www.php.net/manual/fr/function.move-uploaded-file.php la première variable 'imgfile' est le from, la seconde 'imglink' est le to, la fonction moveupladedfile déplace from, to. Ainsi le fichier temp va du dossier tmp de l'ordinateur au chemin indiqué dans to (imglink) donc le dossier image qui a été choisi.
+            $addImage = $this->bdd->prepare("INSERT INTO images (link, id_produit, id_mesure) VALUES (:img_link, $idArt, $typeimage)");
+            $addImage->execute($data);
+            }
+
+
+    }
+
+    public function deleteOneProd($id) {
+        $deleteOneProd = $this->bdd->prepare("DELETE FROM produits WHERE id=$id");
+        $deleteOneProd->execute();
+    }
+
+    //public function modifImgProd($name,)
+        
+
+        public function getAllCat(){
+        $getCat = $this->bdd->prepare("SELECT `categorie`, `id`  FROM `categories`");
+        $getCat->execute();
+        $getAllCat = $getCat->fetchall(PDO::FETCH_ASSOC);
+        return $getAllCat;
+        }
+
+        public function getOneCat($id_categorie){
+        $getCat = $this->bdd->prepare("SELECT * FROM `categories` WHERE `id` = $id_categorie");
+        $getCat->execute();
+        $getCatName = $getCat->fetchall(PDO::FETCH_ASSOC);
+        return $getCatName;
+        var_dump($getCatName);
+        }
+
+
+        
+        public function getAllUser(){
+        $getUser = $this->bdd->prepare("SELECT * FROM `users`");
+        $getUser->execute();
+        $getUsers = $getUser->fetchall(PDO::FETCH_ASSOC);
+        return $getUsers;
+    }
+
+    public function getOneUser($id){
+        $getOneUser = $this->bdd->prepare("SELECT * FROM `users` WHERE `id` = $id");
+        $getOneUser->execute();
+        $getUser = $getOneUser->fetchall(PDO::FETCH_ASSOC);
+        //var_dump($getDoc);
+        return $getUser;
+    }
+
+    public function modifOneUser($id, $login, $email, $id_droit){
+        $modifUser = $this->bdd->prepare("UPDATE users SET login = '$login', email = '$email', id_droit = '$id_droit' WHERE id = $id");
+        $modifUser->execute();
+    }
+
+    public function deleteOneUser($id){
+        $deleteUser = $this->bdd->prepare("DELETE FROM users WHERE id=$id");
+        $deleteUser->execute();
+    }
+
+
+
+    
 
         
 
--- front/admin.php
+++ front/admin.php
@@ -17,9 +17,23 @@
     <main>
     <form action="admin.php" method="get">
   <button name="element" type="submit" value="documents">Documents</button>
+  <button name="element" type="submit" value="utilisateurs">Utilisateurs</button>
   <button name="element" type="submit" value="produits">Produits</button>
+  <button name="element" type="submit" value="creerproduit">Créer un produit</button>
+
     </form>
     <?php 
+
+    if(empty($_GET['element'])) {
+        $_GET['element'] = null;
+    }
+    if((isset($_POST['document'])) && !empty($_POST['document'])) {
+        $contenu = $_POST['document'];
+    }
+    else {
+        $contenu = null;
+    }
+
     if((isset($_GET['modif'])) && !empty($_GET['modif'])) {
         $id = $_GET['modif'];
     }
@@ -29,8 +43,8 @@
     if(isset($_GET['element']) && $_GET['element'] == 'documents' ) { 
         $admincontent = new Admin();
         $getDocs = $admincontent->getAllDoc(); ?>
-                <h1>titre</h1>
-                <table>
+                <h1>Documents</h1>
+                <table class="tableau-style">
                   <thead>
                       <th>id</th>
                       <th>titre</th>
@@ -54,14 +68,119 @@
                       }
                       ?>
                       </table>
-                      <?php } ?>
-
+                      <?php } 
+    ?>
                       <?php 
+                      
+                      if($_GET['element'] == 'document' && $_GET['modif'] !== 0 && !empty($_GET['modif'])) {
+                        $admincontent = new Admin();
+                        $getOnedoc = $admincontent->getOneDoc($id);  
+                        //var_dump($getOnedoc); ?>
+                    <h1>Modification de la documentation</h1>
+                    <form method="POST">
+                      
+                      <textarea name="document" class="texterg">
+                      <?php $document = $getOnedoc[0]['text'];
+                        echo stripslashes($document); ?>
+                      </textarea>
+
+                      <input type="submit" name="modifier" value="Modifier">
+                      </form>
+                    <?php
+
+                    if(isset($_POST['document']) && isset($_POST['modifier'])  && strlen($contenu) <= 10000) {
+                      $contenu = $_POST['document'];
+                      $admincontent = new Admin();
+                      $modifDoc = $admincontent->modifOneDoc($id, $contenu);
+                      }
+
+                    }?>
+
+                    <?php if(isset($_GET['element']) && $_GET['element'] == 'utilisateurs' ) { 
+                    $admincontent = new Admin();
+                    $getUsers = $admincontent->getAllUser(); ?>
+                    <h1>Utilisateurs</h1>
+                    <table class="tableau-style">
+                    <thead>
+                      <th>id</th>
+                      <th>login</th>
+                      <th>email</th>
+                      <th>droits</th>
+                      <th>lien</th>
+
+                      
+                  </thead>
+                  <tbody>
+                      <?php
+                      foreach($getUsers as $getUser){
+                      ?>
+                      <tr>
+                        <td data-label="Date"><?=$getUser['id'] ?></td>
+                          <td data-label="Date"><?=$getUser['login'] ?></td>
+                          <td data-label="Date"><?=$getUser['email'] ?></td>
+                          <td data-label="Date"><?=$getUser['id_droit'] ?></td>
+                          <td> <a href="./admin.php?element=utilisateurs&amp;modif=<?= $getUser['id']; ?>">modification</a></td> 
+
+                      </tr>
+
+                      <?php
+                      //var_dump($getDoc);
+                      }
+                    }
+                      ?>
+                      </table>
+
+    <?php if($_GET['element'] == 'utilisateurs' && isset($_GET['modif'])) {
+        $_GET['modif'] = $id;
+        $admincontent = new Admin();
+        $getUser = $admincontent->getOneUser($id);
+        if(empty($getUser)) {
+            $getUser['0']['id_droit'] = " ";
+            $getUser['0']['login'] = " ";
+            $getUser['0']['email'] = " ";
+            echo "Cet utilisateur n'existe pas";
+        }?>
+        <h1>Modification de l'utilisateur</h1>
+        <form method="POST">
+                      
+                      <label for="login">Login</label>
+                      <input type="text" id="login" name="login" class="form-control" value="<?=$getUser['0']['login'] ?>">
+           
+           
+                      <label for="email">email</label>
+                      <input type="text" id="email" name="email"  class="form-control" value="<?= $getUser['0']['email'] ?>">
+          
+                      <label for="id_droits">id_droits</label>
+                      <input type="text" id="id_droit" name="id_droit"  class="form-control" value="<?= $getUser['0']['id_droit'] ?>">
+   
+                      <input type="submit" name="modifier" value="Modifier">
+                      <input  type="submit" name="supprimer" value="supprimer" method="POST"> 
+
+              </form>
+    <?php if(isset($_POST['modifier']) && !empty($_POST['login']) && !empty($_POST['email']) && !empty($_POST['id_droit'])) {
+        $id = $_GET['modif'];
+        $login = $_POST['login'];
+        $email = $_POST['email'];
+        $id_droit = $_POST['id_droit'];
+        $admincontent = new Admin();
+        $modifUser = $admincontent->modifOneUser($id, $login, $email, $id_droit);
+
+    }
+    if(isset($_POST['supprimer']) && $_GET['modif'] !== 0 && !empty($_GET['modif'])) {
+        $admincontent = new Admin();
+        $deleteUser = $admincontent->deleteOneUser($id);
+        unset($_GET);
+        
+    }
+}
+
+    
+                      
     if(isset($_GET['element']) && $_GET['element'] == 'produits' ) {
         $admincontent = new Admin();
         $getProduits = $admincontent->getAllProd();
  ?>
-                <h1>titre</h1>
+                <h1>Liste des produits</h1>
                 <table>
                   <thead>
                       <th>id</th>
@@ -88,12 +207,142 @@
                       ?>
                       </table>
                       <?php 
+                       if((!isset($_GET['modif'])) && empty($_GET['modif'])) {
+                        $_GET['modif'] = null;
+                    }
                     if($_GET['element'] == 'produits' && $_GET['modif'] !== 0 && !empty($_GET['modif'])) {
                         $admincontent = new Admin();
-                        $getProduit = $admincontent->getOneProd($id);  
+                        $getProduit = $admincontent->getOneProd($id);
+                        $categories = $admincontent->getAllCat();
+                        $id_categorie = $getProduit['0']['id_categorie'];
+                        $getCatName = $admincontent->getOneCat($id_categorie);
+                        var_dump($getCatName[0]);
                         var_dump($getProduit);
+                        
+                        ?>
+                    <h1>Modification d'un produit</h1>
+                    <form method="POST">
+                    <label for="titre">Titre</label>
+                      <input type="text" id="titleprod" name="titleprod" class="form-control" value="<?=$getProduit['0']['titre'] ?>">
+                    <label for="titre">Titre</label>
+                      <input type="text" id="prixprod" name="prixprod" class="form-control" value="<?=$getProduit['0']['prix'] ?>">
+                    <h1></h1>
+                    <select name="categorie">
+                        <option value="<?= $getCatName[0]['id']?>"><?= $getCatName[0]['categorie']?></option>
+                        <?php foreach($categories as $categorie) {           
+                        
+                        ?>
 
-                    }?>
+                        <option  value="<?=$categorie['id']?>"><?=$categorie['categorie']?></option>
+                        <?php }?>
+                        </select>
+
+                    <p>Description du produit:</p>
+                    <textarea id="descriptifprod" name="descriptifprod"><?= $getProduit['0']['descriptif'] ?></textarea> 
+                    <input type="submit" class="boutonvalidation" name="submit2" value="Etape Suivante">
+                    <input  type="submit" name="supprimerprod" value="supprimer" method="POST"> 
+
+
+                        <?php if(isset($_POST['titleprod']) && isset($_POST['prixprod']) && isset($_POST['descriptifprod']) && isset($_POST['categorie']) ) {
+                         $_GET['modif'] = $id;
+                         $prix = $_POST['prixprod'];
+                         $title = $_POST['titleprod'];
+                         $descriptif = $_POST['descriptifprod'];
+                         $categorie = $_POST['categorie'];
+                         $admincontent = new Admin();
+                         $modifOneProd = $admincontent->modifOneProd($id, $prix, $title, $descriptif, $categorie);
+                        }
+                        if(isset($_POST['supprimerprod'])) {
+                        $_GET['modif'] = $id;
+                        $admincontent = new Admin();
+                        $deleteOneProd = $admincontent->deleteOneProd($id);
+                        ?>
+                        <script type="text/javascript">
+                        document.location.href="admin.php?element=produits"
+                        </script>
+                        <?php
+                        }
+
+                    }
+
+                    
+                    if(isset($_GET['element']) && $_GET['element'] == 'creerproduit' ) {
+                        $admincontent = new Admin();
+                        $categories = $admincontent->getAllCat();
+                         ?>
+                         <h1>Création d'un produit</h1>
+                    <form action="" method="POST">
+                        <p>Renseigner le nom du produit:</p>
+                    <input type="text" id="titleprod" name="titleprod">
+                    <p>Prix</p>
+                    <input type="text" id="prixprod" name="prixprod">
+                    <h1></h1>
+                    <select name="categorie">
+                        <?php foreach($categories as $categorie) {?>
+
+                        <option value="<?=$categorie['id']?>"><?=$categorie['categorie']?></option>
+                        <?php }?>
+                        </select>
+
+                    <p>Description du produit:</p>
+                    <textarea id="descriptifprod" name="descriptifprod"></textarea> 
+                    <input type="submit" class="boutonvalidation" name="submit1" value="Etape Suivante">
+
+                    
+
+                    <?php if(isset($_POST['titleprod']) && isset($_POST['prixprod']) && isset($_POST['descriptifprod']) && isset($_POST['categorie']) ) {
+                    $prix = $_POST['prixprod'];
+                    $title = $_POST['titleprod'];
+                    $descriptif = $_POST['descriptifprod'];
+                    $categorie = $_POST['categorie'];
+                    $admincontent = new Admin();
+                    $addOneProd = $admincontent->addOneProd($prix, $title, $descriptif, $categorie);
+                    //var_dump($_POST);
+                    //var_dump($prix);
+
+
+                     } }
+
+                     if(isset($_POST['submit1']) ) {
+                        header('Location: ./admin.php?element=imageproduits&name='.$title);}
+                    if(isset($_GET['element']) && $_GET['element'] == 'imageproduits') {    
+                    ?>
+                     <form action="" method="post" enctype="multipart/form-data">
+                    <label for="photo">Choisir une image</label>
+                    <input type="file" accept="image/png, image/jpeg, image/webp" name="image">
+                    <input type="radio" name="typeimage" type="checkbox" id="typeimage" value="1">Ceci est une image produit
+                    <input type="radio" name="typeimage" type="checkbox" id="typeimage" value="2">Ceci est une image de mesure
+                    <button type="submit" name="addimages">Envoyer</button>
+                    </form>
+
+
+                    <?php
+                    $name = $_GET['name'];
+                    if(isset($_POST['addimages']) && isset($_POST['typeimage']) && $_FILES['image']['size'] > 0){//la super global FILE correspond à un fichier temporaire stocker dans C:\wamp64\tmp\phpE493.tmp elle contient les détails du fichier (poid, nom ect...)
+
+                        //ces informations sont stocké dans un array (deux dans le cas présent pour faire matcher le nombre de valeur de l'arrêt au nombre de valeur attendu dans la requête mysl et ainsi éviter une erreur) pour être envoyé
+                            $dataImage = [
+                                'img_link' => './images/' . $_FILES['image']['name'],
+                                'img_file' => $_FILES['image']['tmp_name']
+                            ];
+                            $data = [
+                                'img_link' => $dataImage['img_link']
+                            ];
+                        
+                            $typeimage = $_POST['typeimage'];
+                            echo $typeimage;
+                            $admincontent = new Admin();
+                            $imgProd = $admincontent->imgProd($name, $dataImage, $data, $typeimage);
+
+                        }
+
+                    }
+                    
+                     ?>
+
+
+                    
+                    
                       
                       
     </main>
